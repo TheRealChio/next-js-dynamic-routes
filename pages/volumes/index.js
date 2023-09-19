589a8b3e22abd68f8caad4306c730ae73894773b
Daniel B <d.berneisch@gmx.de>
2023-09-19 15:54:41 +0200

    initial

--- pages/volumes/index.js
+++ pages/volumes/index.js
@@ -1,26 +1,35 @@
 import Link from "next/link";
 import { introduction } from "../../lib/data";
+import { volumes } from "../../lib/data";
+import { router } from "next/router";
 
 export default function Volumes() {
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
   return (
     <>
       <h1>The Lord of the Rings</h1>
       <p>{introduction}</p>
       <ul>
-        <li>
-          <Link href="/volumes/the-fellowship-of-the-ring">
-            The Fellowship of the Ring
-          </Link>
-        </li>
-        <li>
-          <Link href="/volumes/the-two-towers">The Two Towers</Link>
-        </li>
-        <li>
-          <Link href="/volumes/the-return-of-the-king">
-            The Return of the King
-          </Link>
-        </li>
+        {volumes.map((volume) => {
+          return (
+            <li key={volume.slug}>
+              <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+            </li>
+          );
+        })}
       </ul>
+      <button
+        type="button"
+        onClick={() => {
+          const { slug } = getRandomElement(volumes);
+          router.push(`/volumes/${slug}`);
+        }}
+      >
+        Random Volume
+      </button>
     </>
   );
 }
